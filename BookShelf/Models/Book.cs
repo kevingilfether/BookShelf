e985ee6f0f970a5a7718e2909b7c6c915240cba3
kevingilfether <kevingilfether@gmail.com>
2017-10-26 10:41:10 -0400

    Set up models for book and category

---
+++ BookShelf/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookShelf.Models
+{
+    public class Book
+    {
+        [Key]
+        public int ID { get; set; }
+        [MinLength(2),MaxLength(50)]
+        public string Title { get; set; }
+        public DateTime PublishedDate { get; set; }
+        [MaxLength(500)]
+        public string Description { get; set; }
+        public string ISBN { get; set; }
+
+        public string Category { get; set; }
+
+    }
+}
